fc4e907d1f2659f659fae20d786c57721faf0aae
lyzhuboyang <lyzhuboyang@gmail.com>
2020-07-23 12:04:52 +0800

    1111

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -4,10 +4,5 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("11111");
-		System.out.println("44444");
-		System.out.println("22222");
-		System.out.println("33333");
-		System.out.println("55555");
-		System.out.println("66666");
 	}	
 }
